a24cea024d5934249f7b904521a1bf997acc8025
Jacek Nakonieczny <jacek.nakonieczny@gmail.com>
2014-07-29 09:22:12 +0200

    Add method to file bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-if [ -d ../.weechat ]; then
-	rmdir ../.weechat
-elif [ -L ../.weechat ]; then
-	rm ../.weechat
-fi
-ln -s `pwd`/.weechat ../.weechat
+
+connect_config()
+{
+    if [ -d $2 ]; then
+      echo Please remove existing config directory $2
+      return 1
+    elif [ -L $2 ]; then
+      echo Please remove existing link to config $2
+      return 1
+    fi
+    ln -s `pwd`/$1 $2
+}
+
+connect_config sublime-text-3 $HOME/.config/sublime-text-3
+connect_config .weechat $HOME/.weechat
